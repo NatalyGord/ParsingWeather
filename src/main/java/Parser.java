6129d62972c49d67f2dc79c85554e35827494b11
Natalia Gordiyets <NatGord@rambler.ru>
2021-10-27 23:55:01 +0700

    Вывод значений погоды и корректировка вывода если не все времена суток есть в текущей дате

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -15,16 +15,57 @@ public class Parser {
         return pageCode;
     }
 
+    public static int printValues(Elements values, int index){
+        int countIter = 4;
+        if (index == 0) {
+            Element valueLn = values.get(3);
+            boolean isNight = valueLn.text().contains("Ночь");
+            boolean isMorning = valueLn.text().contains("Утро");
+            boolean isDay = valueLn.text().contains("День");
+            boolean isEvening = valueLn.text().contains("Вечер");
+
+            if (isNight) {
+                countIter = 3;
+            } else if (isDay) {
+                countIter = 1;
+            }else if(isMorning){
+                countIter = 2;
+            }
+        }
+        for (int i = 0; i < countIter; i++){
+            Element valueLine = values.get(index + i);
+            for (Element td: valueLine.select("td")){
+                System.out.print(td.text() + "        ");
+            }
+            System.out.println();
+        }
+        return countIter;
+    }
+
     public static void main(String[] args) throws IOException {
 //        System.out.println(getPageCode());
         Document pageCode = getPageCode();
+        String nameDate = "";
+        int index = 0;  //показывает где мы сейчас находимся
 
         Element pageCodeWeather = pageCode.selectFirst("div[class=block rc5 b2]");
-        System.out.println(pageCodeWeather);
+//        System.out.println(pageCodeWeather);
         Elements nameColumns = pageCodeWeather.select("tr[height=20px]");
 //        System.out.println(nameColumns);
+        String nameDateString = pageCodeWeather.text();
+//        System.out.println(nameDateString);
+
         Elements values = pageCodeWeather.select("tr[height=125px]");
 //        System.out.println(values);
+
+        for (Element nameColumn: nameColumns){
+
+            System.out.println();
+            System.out.println(nameDate + "             осадки                     температура        ветер             влажность        давление");
+
+            int countIter = printValues(values, index);
+            index = index + countIter;
+        }
 
     }
 }
