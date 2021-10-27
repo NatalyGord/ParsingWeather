e804107f5cd1546362f3b9ac901db1628d4cc047
Natalia Gordiyets <NatGord@rambler.ru>
2021-10-27 13:39:39 +0700

    Получение кода всей страницы

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class Parser {
+
+
+    public static Document getPageCode() throws IOException {
+        String url = "https://pogodavtomske.ru/threeday.html";
+        Document pageCode = Jsoup.parse(new URL(url), 3000);
+        return pageCode;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getPageCode());
+    }
 }
