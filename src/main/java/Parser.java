da755867e5cf0967fc69eaba0b9410d82ba6decc
Natalia Gordiyets <NatGord@rambler.ru>
2021-10-28 15:13:17 +0700

    Вытаскивание даты из строки с помощью регулярных выражений и добавление ее в таблицу

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,6 +5,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
 
@@ -15,56 +17,69 @@ public class Parser {
         return pageCode;
     }
 
-    public static int printValues(Elements values, int index){
+    private static Pattern pattern = Pattern.compile("[А-Я][а-я]{4,}\\s\\d{2}\\s[а-яА-Я]+"); //Четверг 28 октября
+
+    public static String[] getDate(String nameDateString) throws Exception {
+        int count = 0;
+        String[] allMatches = new String[3];
+        Matcher matcher = pattern.matcher(nameDateString);
+
+        while (matcher.find()) {
+            allMatches[count] = matcher.group();
+            count++;
+        }
+        return allMatches;
+    }
+
+    public static int printValues(Elements values, int index) {
         int countIter = 4;
         if (index == 0) {
             Element valueLn = values.get(3);
             boolean isNight = valueLn.text().contains("Ночь");
             boolean isMorning = valueLn.text().contains("Утро");
             boolean isDay = valueLn.text().contains("День");
-            boolean isEvening = valueLn.text().contains("Вечер");
 
             if (isNight) {
                 countIter = 3;
             } else if (isDay) {
                 countIter = 1;
-            }else if(isMorning){
+            } else if (isMorning) {
                 countIter = 2;
             }
         }
-        for (int i = 0; i < countIter; i++){
+        for (int i = 0; i < countIter; i++) {
             Element valueLine = values.get(index + i);
-            for (Element td: valueLine.select("td")){
-                System.out.print(td.text() + "        ");
+            for (Element td : valueLine.select("td")) {
+                System.out.print(td.text() + "\t\t\t\t\t");
             }
             System.out.println();
         }
         return countIter;
     }
 
-    public static void main(String[] args) throws IOException {
-//        System.out.println(getPageCode());
+    public static void main(String[] args) throws Exception {
         Document pageCode = getPageCode();
-        String nameDate = "";
         int index = 0;  //показывает где мы сейчас находимся
 
         Element pageCodeWeather = pageCode.selectFirst("div[class=block rc5 b2]");
-//        System.out.println(pageCodeWeather);
         Elements nameColumns = pageCodeWeather.select("tr[height=20px]");
-//        System.out.println(nameColumns);
-        String nameDateString = pageCodeWeather.text();
+
+        String nameDateString = pageCodeWeather.text(); //строка из которой выбираем даты
+        String[] nameDate = getDate(nameDateString);  //массив найденых дат
+
 //        System.out.println(nameDateString);
-
+//        for (String n : nameDate) {
+//            System.out.println(n);
+//        }
         Elements values = pageCodeWeather.select("tr[height=125px]");
-//        System.out.println(values);
-
-        for (Element nameColumn: nameColumns){
-
+        int itr = 0;  //Выбор элемента из массива найденых дат
+        for (Element nameColumn : nameColumns) {
             System.out.println();
-            System.out.println(nameDate + "             осадки                     температура        ветер             влажность        давление");
+            System.out.println(nameDate[itr] + "\t\t\tосадки\t\t\t\t\t\t\t\tтемпература\t\t\t\t\tветер\t\t\t\t\tвлажность\t\t\tдавление");
 
             int countIter = printValues(values, index);
             index = index + countIter;
+            itr++;
         }
 
     }
